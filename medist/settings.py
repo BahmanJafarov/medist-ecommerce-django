7e0f7173b799e64b3903d657a45522defaffaaec
BahmanJafarov <jrbahmanjafarov@gmail.com>
2025-01-29 20:28:04 +0400

    add models

--- medist/settings.py
+++ medist/settings.py
@@ -119,6 +119,9 @@ USE_I18N = True
 USE_TZ = True
 
 
+AUTH_USER_MODEL = 'account.User'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
--- product/models.py
+++ product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductCategory(models.Model):
+    parent = models.ForeignKey('self', related_name='child', on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        if self.parent:
+            return (f"{self.parent} - {self.title}")
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = 'Product Categories'
