0cb6bac9b7b94c0e7ae76caa48e69084de1f43d0
BahmanJafarov <jrbahmanjafarov@gmail.com>
2025-01-31 17:00:09 +0400

    blog templates

--- blog/urls.py
+++ blog/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from blog.views import *
 
 
 urlpatterns = [
-    path()
+    path('article/', article, name = 'article'),
+    path('blog/', blog, name = 'blog'),
+    path('search-blog/', search_blog, name = 'search-blog')
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def article(request):
-    return render(request, )
+    return render(request, 'article.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def search_blog(request):
+    return render(request, 'search-blog.html')
