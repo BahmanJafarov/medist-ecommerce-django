1ac99723e66ed3829e808fe6cab536197c99ea51
BahmanJafarov <jrbahmanjafarov@gmail.com>
2025-02-01 16:18:17 +0400

    order templates

--- order/urls.py
+++ order/urls.py
@@ -14,9 +14,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from order.views import *
 
 urlpatterns = [
+    path('cart-drawer-empty/', cart_drawer_empty, name='cart-drawer-empty'),
+    path('cart-empty/', cart_empty, name='cart-empty'),
+    path('cart-page/', cart_page, name='cart-page'),
+    path('checkout/', checkout, name='checkout'),
+    path('invoice/', invoice, name='invoice'),
+    path('order-complete/', order_complete, name='order-complete'),
+    path('order-info-cancel/', order_info_cancel, name='order-info-cancel'),
+    path('order-info-default/', order_info_default, name='order-info-default'),
+    path('order-info-delivered/', order_info_delivered, name='order-info-delivered'),
+    path('order-info-fulfilled/', order_info_fulfilled, name='order-info-fulfilled'),
+    path('order-info-indelivery/', order_info_indelivery, name='order-info-indelivery'),
+    path('order-info-inprogress/', order_info_inprogress, name='order-info-inprogress'),
+    path('order-info-intransit/', order_info_intransit, name='order-info-intransit'),
+    path('order-info-pickup/', order_info_pickup, name='order-info-pickup'),
+    path('order-info-unfulfilled/', order_info_unfulfilled, name='order-info-unfulfilled'),
+    path('order-info/', order_info, name='order-info'),
+    path('order/', order, name='order'),
+    path('track-order/', track_order, name='track-order'),
+    path('wishlist-empty/', wishlist_empty, name='wishlist-empty'),
+    path('wishlist/', wishlist, name='wishlist')
 ]
--- order/views.py
+++ order/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def cart_drawer_empty(request):
+    return render(request, 'cart-drawer-empty.html')
+
+def cart_empty(request):
+    return render(request, 'cart-empty.html')
+
+def cart_page(request):
+    return render(request, 'cart-page7.html')
+
+def checkout(request):
+    return render(request, 'checkout2.html')
+
+def invoice(request):
+    return render(request, 'invoice4.html')
+
+def order_complete(request):
+    return render(request, 'order-complete.html')
+
+def order_info_cancel(request):
+    return render(request, 'order-info-cancel.html')
+
+def order_info_default(request):
+    return render(request, 'order-info-default.html')
+
+def order_info_delivered(request):
+    return render(request, 'order-info-delivered.html')
+
+def order_info_fulfilled(request):
+    return render(request, 'order-info-fulfilled.html')
+
+def order_info_indelivery(request):
+    return render(request, 'order-info-indelivery.html')
+
+def order_info_inprogress(request):
+    return render(request, 'order-info-inprogress.html')
+
+def order_info_intransit(request):
+    return render(request, 'order-info-intransit.html')
+
+def order_info_pickup(request):
+    return render(request, 'order-info-pickup.html')
+
+def order_info_unfulfilled(request):
+    return render(request, 'order-info-unfulfilled.html')
+
+def order_info(request):
+    return render(request, 'order-info.html')
+
+def order(request):
+    return render(request, 'order.html')
+
+def track_order(request):
+    return render(request, 'track-order.html')
+
+def wishlist_empty(request):
+    return render(request, 'wishlist-empty.html')
+
+def wishlist(request):
+    return render(request, 'wishlist.html')
